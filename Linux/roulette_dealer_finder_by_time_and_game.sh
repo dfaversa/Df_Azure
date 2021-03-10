395e1be3c8d1186da4d654f65597e13f49b654ec
dfaversa <73912396+dfaversa@users.noreply.github.com>
2021-03-10 18:38:16 -0500

    Create roulette_dealer_finder_by_time_and_game.sh

---
+++ Linux/roulette_dealer_finder_by_time_and_game.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $1 -eq 1 ]
+then
+
+	grep "$2 $3" *$4* | awk '{print $3, $4}'
+
+elif [ $1 -eq 2 ]
+then
+
+	grep "$2 $3" *$4* | awk '{print $5, $6}'
+
+elif [ $1 -eq 3 ]
+then
+
+	grep "$2 $3" *$4* | awk '{print $7, $8}'
+fi
